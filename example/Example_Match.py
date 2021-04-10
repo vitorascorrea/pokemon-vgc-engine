c7aeb4acf9f0aaf96b9366c8108071248309a7f4
Vitor <vitorascorrea@gmail.com>
2021-04-10 12:30:53 -0300

    Create Greedy competitor file

--- example/Example_Match.py
+++ example/Example_Match.py
@@ -1,10 +1,10 @@
+from competitor.greedy.Greedy import Greedy
 from framework.competition.CompetitionObjects import Match, ExampleCompetitor, GUIExampleCompetitor
 from framework.util.PkmTeamGenerators import RandomGenerator
 
-
 def main():
     rg = RandomGenerator()
-    c0 = GUIExampleCompetitor(rg.get_team(), name="Player0")
+    c0 = Greedy(team=rg.get_team(), name="Player0")
     c1 = ExampleCompetitor(rg.get_team(), name="Player1")
     m = Match(c0, c1, debug=True)
     m.run()
